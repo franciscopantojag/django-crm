32de3c9d2b77208c4350ec78dc78555d1657509b
Francisco Pantoja <franciscopantojag98@gmail.com>
2023-06-14 17:07:48 -0500

    feat: add auth

--- website/views.py
+++ website/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponseNotAllowed
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 
-def home(request):
-    return render(request, "home.html", {})
+def home(request: 'HttpRequest'):
+    return render(request, 'home.html', {})
+
+
+def login_user(request: 'HttpRequest'):
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+    username = request.POST['username']
+    password = request.POST['password']
+
+    user = authenticate(request, username=username, password=password)
+
+    if user is None:
+        messages.success(
+            request, 'There was an Error logging in, please try again')
+        return redirect('home')
+
+    login(request, user)
+    messages.success(request, 'You have been logged in')
+    return redirect('home')
+
+
+def logout_user(request: 'HttpRequest'):
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('home')
