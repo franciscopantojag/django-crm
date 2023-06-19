a0e465348d833177c0a209f7a88e7a044c9a84d3
Francisco Pantoja <franciscopantojag98@gmail.com>
2023-06-19 10:42:55 -0500

    feat: setup view records on website

--- website/views.py
+++ website/views.py
@@ -3,18 +3,20 @@ from django.http import HttpRequest, HttpResponseNotAllowed
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 
 def home(request: 'HttpRequest'):
-    return render(request, 'home.html', {})
+    records = Record.objects.all() if request.user.is_authenticated else None
+    return render(request, 'home.html', {'records': records})
 
 
 def login_user(request: 'HttpRequest'):
     if request.method != 'POST':
         return HttpResponseNotAllowed(['POST'])
+
     username = request.POST['username']
     password = request.POST['password']
-
     user = authenticate(request, username=username, password=password)
 
     if user is None:
